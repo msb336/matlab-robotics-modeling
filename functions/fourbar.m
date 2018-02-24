1fd3172020a5f4b10fdd0e187c88abbbd8381cd5
Matthew Brown <mattbrown@Matthews-MBP.fios-router.home>
2018-02-24 17:50:53 -0500

    Working Commit
    Everything works. Now to generalize...

--- functions/fourbar.m
+++ functions/fourbar.m
@@ -1,4 +1,4 @@
-function [T] = fourbar(T, points, joints, f,j, x, axis)
+function [T] = fourbar(T, points, joints, f,j, x, axis, full)
 %%FOURBAR returns transformation matrices for four bar linkage where
 %%opposite links are parallel
 if any(joints == 3)
@@ -6,8 +6,10 @@ end
 
 T{f} = T{j}*rotate(x,axis)*translate(points(f,:)-points(j,:))*rotate(-x,axis);
 i = joints(joints~=f & joints~=j);
-
-T{i(1)} = T{i(1)}*T{j}*translate(points(i(1),:)-points(j,:));
+if ~full(i(1))
+    T{i(1)} = T{i(1)}*T{j}*translate(points(i(1),:)-points(j,:));
+end
+    
 T{i(2)} = T{i(2)}*T{i(1)}*rotate(x,axis)*translate(points(i(2),:)-points(i(1),:));
 
 
